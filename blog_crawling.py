9df361e0ac8ca2920cb2d9b108128f81eaa657ed
Myeonghyeon Ryu <59256704+RYU-BB@users.noreply.github.com>
2021-07-09 20:13:38 +0900

    Update blog_crawling.py

--- blog_crawling.py
+++ blog_crawling.py
@@ -1,7 +1,11 @@
+import chromedriver_autoinstaller
 from web_crawling.naver_blog.naver_blog_crawling import BlogCrawler
+
 
 keyword = input('키워드를 입력하세요 :')
 page_no = int(input('페이지수를 입력하세요. (한 페이지에 총 7개의 블로그에 대한 정보가 담겨있습니다) :'))
 
-blog_crawler = BlogCrawler(keyword, page_no, 'chromedriver.exe')
+chrome_ver = chromedriver_autoinstaller.get_chrome_version().split('.')[0]
+
+blog_crawler = BlogCrawler(keyword, page_no, f'./{chrome_ver}/chromedriver.exe')
 blog_crawler()
