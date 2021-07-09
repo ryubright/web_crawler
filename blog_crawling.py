f5bdf0ff357bc7a82a2ccc7f1c91a8dfa1adac75
Myeonghyeon Ryu <59256704+RYU-BB@users.noreply.github.com>
2021-07-09 19:58:33 +0900

    main crawling file

---
+++ blog_crawling.py
@@ -0,0 +1,7 @@
+from web_crawling.naver_blog.naver_blog_crawling import BlogCrawler
+
+keyword = input('키워드를 입력하세요.')
+page_no = input('페이지수를 입력하세요. 한 페이지에 총 7개의 블로그에 대한 정보가 담겨있습니다.')
+
+blog_crawler = BlogCrawler(keyword, page_no, '../chromedriver.exe')
+blog_crawler()
