66dcf58147d3991e3c67c0f50f5844351967d653
Myeonghyeon Ryu <59256704+RYU-BB@users.noreply.github.com>
2021-07-10 20:09:22 +0900

    add result msg

--- web_crawling/tasks/naver_blog.py
+++ web_crawling/tasks/naver_blog.py
@@ -9,8 +9,8 @@ class NaverBlogCrawler(CrawlingBase):
         super(NaverBlogCrawler, self).__init__(keyword, max_page_no, driver_path)
 
     def __call__(self) -> None:
-        blog_title, blog_contents = self._crawling()
-        self._to_csv(blog_title, blog_contents)
+        blog_titles, blog_contents = self._crawling()
+        self._to_csv(blog_titles, blog_contents)
 
     @property
     def get_url(self) -> str:
@@ -34,26 +34,26 @@ class NaverBlogCrawler(CrawlingBase):
 
             try:
                 for blog_no in range(1, 8):
-                    blog_title = self.driver.find_element_by_xpath(f'//*[@id="content"]/section/div[2]/div[{blog_no}]'
-                                                                   f'/div/div[1]/div[1]/a[1]/strong/span')
+                    blog_title = self._driver.find_element_by_xpath(f'//*[@id="content"]/section/div[2]/div[{blog_no}]'
+                                                                    f'/div/div[1]/div[1]/a[1]/strong/span')
 
-                    blog_content = self.driver.find_element_by_xpath(f'//*[@id="content"]/section/div[2]/div[{blog_no}]'
-                                                                     f'/div/div[1]/div[1]/a[2]')
+                    blog_content = self._driver.find_element_by_xpath(f'//*[@id="content"]/section/div[2]/'
+                                                                      f'div[{blog_no}]/div/div[1]/div[1]/a[2]')
 
                     blog_title_list.append(blog_title.text)
                     blog_content_list.append(blog_content.text)
-
             except:
                 self.error_count += 1
                 print(f"{page_no}번째 page {blog_no}번째 게시글에서 오류발생")
 
         return blog_title_list, blog_content_list
 
-    def _to_csv(self, title, contents):
-        blog_text_df = pd.DataFrame({'blog_title': title, 'blog_content': contents})
+    def _to_csv(self, titles, contents):
+        blog_text_df = pd.DataFrame({'blog_title': titles, 'blog_content': contents})
         blog_text_df.to_csv(
             f'naver_blog_text_data{self.max_page_no * 7 - self.error_count}.csv',
             header=True,
             index=True,
             encoding='utf-8-sig'
         )
+        print(f"총 {self.max_page_no * 7 - self.error_count}개의 블로그 데이터 수집 완료")
