18b68cb88a67599a1e01edd4df04b2ff2fa11060
Myeonghyeon Ryu <59256704+RYU-BB@users.noreply.github.com>
2021-07-11 23:29:47 +0900

    Update __init__.py

--- web_crawler/__init__.py
+++ web_crawler/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from web_crawler.crawling import WebCrawler
+from web_crawler.crawler import WebCrawler
